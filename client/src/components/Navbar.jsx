0ecc78962cfa12435db5cad6d6d0eaa0f5c2a3dc
Dozie2001 <davidchidozie402@gmail.com>
2023-11-14 23:14:04 +0100

    NavBar adjustment and images

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,25 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+
+const NavbarItem = ({ title, classProps }) => {
+    return(
+        <li classname={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    );
+}
 const Navbar = () => {
     return (
-        <h1>Nav Bar</h1>
+        <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+            <div  className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className='w-32 cursor pointer'></img>
+            </div>
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+                <NavbarItem />
+            </ul>
+        </nav>
     )
 }
 
