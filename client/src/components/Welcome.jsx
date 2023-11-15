3fdd3e8b7dac92ad4daddddce438fcbd976af9ac
Dozie2001 <davidchidozie402@gmail.com>
2023-11-15 14:08:34 +0100

    Adjustments to Welcome Bar

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,20 @@
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from 'react-icons/bs';
+
+import { Loader } from './';
+
 const Welcome = () => {
     return (
-        <h1>Welcome</h1>
+       <div className="flex w-full justify-center items-center">
+        <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-1 px-4">
+            <div className="flex flex-1 justify-start  flex-col md:mr">
+                <h1 className="text-3xl sm:text-5xl text-white gradient py-1">
+                    Send Crypto <br /> Everywhere
+                </h1>
+            </div>
+        </div>
+       </div>
     )
 }
 
