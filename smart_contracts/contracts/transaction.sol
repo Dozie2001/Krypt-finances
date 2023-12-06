5a74c790c530f0c4fa26a90c2ce9186f576b4434
Dozie2001 <davidchidozie402@gmail.com>
2023-12-06 16:20:10 +0100

    Adjustment to contract

--- smart_contracts/contracts/transaction.sol
+++ smart_contracts/contracts/transaction.sol
@@ -1,43 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
-// @title A Transaction script to the blockchain
-//@author Dozie
-//@dev All function calls are currently implemented without side effects
+import "hardhat/console.sol";
+
 contract Transactions {
     uint256 transactionCount;
 
-    event Transfer (address from, address _to, uint amount, string message, uint256 timestamp, string keyword);
-
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+  
     struct TransferStruct {
-        address from;
-        address _to;
-        uint _amount;
-        string _message;
-        uint256 _timestamp;
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
         string keyword;
     }
 
     TransferStruct[] transactions;
 
-
-    function addToBlockChain(address payable _to, uint _amount, string memory _message, string memory keyword) public {
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
         transactionCount += 1;
-        transactions.push(TransferStruct(msg.sender, _to, _amount, _message, block.timestamp, keyword));
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
-        emit Transfer(msg.sender, _to, _amount, _message, block.timestamp, keyword);
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
 
-
-    // Transaction getter function
-    // @dev Return Transaction
-    function getAllTransactions() public view returns(TransferStruct[] memory) {
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
         return transactions;
-
     }
 
-    // @dev return transaction count
-    function getTransactionCount() public view returns(uint256) {
+    function getTransactionCount() public view returns (uint256) {
         return transactionCount;
     }
 }
